a92d243450ff013d684107c834dc2601d3c9c4c2
Kushal-Ajay-Shah <shahkushal0602@gmail.com>
2020-06-10 20:38:00 +0530

    Wrapper added, back functionality improved, logged in user can directly go to home screen

---
+++ kitchen_mate/lib/screens/wrapper.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:kitchen_mate/screens/home_screen.dart';
+import 'package:kitchen_mate/screens/welcome_screen.dart';
+import 'package:kitchen_mate/models/user.dart';
+class Wrapper extends StatelessWidget {
+  static const String id = 'wrapper';
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print(user);
+    if(user == null){
+      return WelcomeScreen();
+    }else{
+      return Home();
+    }
+  }
+}
---
+++ kitchen_mate/lib/services/auth.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:kitchen_mate/models/user.dart';
+
+
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  // create user object based on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user){
+    return user != null ? User(uid: user.uid) : null;
+  } 
+  
+
+  // auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+    .map(_userFromFirebaseUser);
+  }
+
+
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try{
+      AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //register with email and password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try{
+      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+
+      //create a new document for the user with uid
+      // await DatabaseService(uid: user.uid).updateUserData('0', 'new crew member', 100);
+      // return _userFromFirebaseUser(user);
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
+
+
+  //sign out
+  Future signOut() async {
+    try{
+      return await _auth.signOut();
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+}
