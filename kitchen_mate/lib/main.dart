01215aefc1cc6a6d42b528d89834343ea2c10347
Kushal-Ajay-Shah <shahkushal0602@gmail.com>
2020-06-28 20:39:17 +0530

    Orientation problem solved and login page logo restored

--- kitchen_mate/lib/main.dart
+++ kitchen_mate/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kitchen_mate/screens/days_of_week.dart';
 
 import 'package:kitchen_mate/screens/wrapper.dart';
@@ -22,6 +23,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ]);
     return StreamProvider<User>.value(
           value: AuthService().user,
           child: MaterialApp(
